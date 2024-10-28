31cc211dc1f330754fb8053cc7e3ec514cc71149
Maksymilian Kliczkowski <maksymilian.kliczkowski@pwr.edu.pl>
2024-10-28 11:54:40 +0100

    maath

--- src/Include/maths.h
+++ src/Include/maths.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #ifndef MATH_H
 #define MATH_H
 
@@ -26,9 +27,7 @@
 								std::is_base_of<long, _T>::value								||
 								std::is_base_of<unsigned long, _T>::value						||
 								std::is_base_of<long long, _T>::value							||
-								std::is_base_of<unsigned long long, _T>::value					||
-								std::is_base_of<uint_fast16_t, _T>::value						||
-								std::is_base_of<uint_fast32_t, _T>::value;
+								std::is_base_of<unsigned long long, _T>::value;
 #	endif
 #endif
 /*******************************
@@ -134,5 +133,91 @@ namespace Math
 		return std::round(_val * (_P + 1)) / (_P + 1);
 	}
 }
+
+// ###############################################################################
+
+
+#include <future>
+#include <vector>
+#include <atomic>
+#include <functional>
+namespace Threading
+{
+	/*
+	* @brief Creates futures for a class member function with arguments and returns the results.
+	* 
+	* @tparam ClassType Type of the class instance.
+	* @tparam _R Return type of the function.
+	* @tparam _F Type of the member function. The function necessarlily has to
+	* 	take the following arguments: size_t, size_t, std::atomic<size_t>&, size_t, _Args... and return _R.
+	*	the first two arguments are the start and stop indices of the iteration, the third is the total number of elements,
+	*	the fourth is the thread number, and the rest are the additional arguments.
+	* @tparam _Args Types of the additional arguments for the function.
+	* 
+	* @param _instance Pointer to the class instance.
+	* @param _totalIterator Atomic iterator for the total number of elements.
+	* @param _thNum Number of threads to be used.
+	* @param _isParallel Indicates whether execution is parallel.
+	* @param _iterSize Total size of the iteration.
+	* @param f Member function pointer.
+	* @param args Arguments for the member function.
+	* @returns A vector containing results of type _R.
+	*/
+	template <typename ClassType, typename _R, typename _F, typename ..._Args>
+	inline std::vector<_R> createFutures(ClassType* _instance,							// instance of the class
+										std::atomic<size_t>& _totalIterator,			// iterator for the total number of elements
+										size_t _thNum,									// number of threads		
+										bool _isParallel,								// is parallel				
+										size_t _iterSize,								// size of the iteration			
+										_F &&f, _Args &&...args)						// function and arguments		 
+					
+	{
+		if (!_isParallel)
+			_thNum = 1;
+		
+		// reserve the threads
+		std::vector<std::future<_R>> _futures;
+		_futures.reserve(_thNum);
+
+		for(auto _ithread = 0; _ithread < _thNum; ++_ithread)
+		{
+			// get the subvectors
+			size_t _startElem	= _ithread * _iterSize / _thNum;
+			size_t _stopElem	= (_ithread + 1) * _iterSize / _thNum;
+
+			// Validate indices
+			if (_startElem >= _iterSize || _stopElem > _iterSize)
+				throw std::out_of_range("Thread indices out of range");
+
+			_futures.push_back(
+				std::async(
+					[=, &_totalIterator, &_instance]() mutable { 
+						// Use mutable lambda to avoid const issues
+						// Call the member function with all arguments
+						return (_instance->*f)(_startElem, 
+												_stopElem, 
+												std::ref(_totalIterator), 
+												_ithread,
+												std::forward<_Args>(args)...);
+					}
+				)
+			);
+		}
+
+		std::vector<_R> _out;
+		for (auto& future : _futures) {
+			try {
+				auto _res = future.get();
+				_out.push_back(_res);
+			} catch (const std::exception& e) {
+				std::cerr << "Exception while getting future result: " << e.what() << std::endl;
+			}
+		}
+		return _out;
+	}
+
+
+};
+
 
 #endif
