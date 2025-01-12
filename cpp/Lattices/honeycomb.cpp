e092ef5af0e8a4cdad2f4b32c6333d6f76e69375
makskliczkowski <maxgrom97@gmail.com>
2025-01-12 20:19:21 +0100

    Refactor Honeycomb::get_flux_sites to use explicit uint type for node variables; add debug output for flux sites and comment out unused node_left_2.

--- cpp/Lattices/honeycomb.cpp
+++ cpp/Lattices/honeycomb.cpp
@@ -316,32 +316,38 @@ v_1d<uint> Honeycomb::get_flux_sites(int X, int Y, int Z) const
 	}
 
 	// calculate the starting point 
-	const auto node_start = 2 * (X + Lx * Y + Lx * Ly * Z) + 1;
+	const uint node_start = 2 * (X + Lx * Y + Lx * Ly * Z) + 1;
 	_flux_sites.push_back(node_start);
 
 	// get neighbor up
-	const auto node_up 		= this->nn[node_start][0];
+	const uint node_up 		= this->nn[node_start][0];
 	_flux_sites.push_back(node_up);
 
 	// get neighbor right (x bond)
-	const auto node_right 	= (node_up > 0) ? this->nn[node_up][2] : -1;
+	const uint node_right 	= (node_up > 0) ? this->nn[node_up][2] : -1;
 	_flux_sites.push_back(node_right);
 
 	// get neighbor right again (y bond)
-	const auto node_right_2 = (node_right > 0) ? this->nn[node_right][1] : -1;
+	const uint node_right_2 = (node_right > 0) ? this->nn[node_right][1] : -1;
 	_flux_sites.push_back(node_right_2);
 
 	// get neighbor down (z bond)
-	const auto node_down 	= (node_right_2 > 0) ? this->nn[node_right_2][0] : -1;
+	const uint node_down 	= (node_right_2 > 0) ? this->nn[node_right_2][0] : -1;
 	_flux_sites.push_back(node_down);
 
 	// get neighbor left (x bond)
-	const auto node_left 	= (node_down > 0) ? this->nn[node_down][2] : -1;
+	const uint node_left 	= (node_down > 0) ? this->nn[node_down][2] : -1;
 	_flux_sites.push_back(node_left);
 
 	// get neighbor left again (y bond)
-	const auto node_left_2 	= (node_left > 0) ? this->nn[node_left][1] : -1;
-	_flux_sites.push_back(node_left_2);
+	// const uint node_left_2 	= (node_left > 0) ? this->nn[node_left][1] : -1;
+	// _flux_sites.push_back(node_left_2);
+
+    // fixme
+    // _flux_sites = {node_up, node_right, node_right_2, node_down, node_left, node_left_2};
+
+    for (int i = 0; i < _flux_sites.size(); ++i)
+        std::cout << std::format("Flux {} : {}\n", i, _flux_sites[i]);
 
 	return _flux_sites;
 }
